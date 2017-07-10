8cc90bf70d6c508a5d9350a2814509cd25227d61
Lien <messagelien@gmail.com>
2017-07-10 22:38:30 +1000

    create test file and create working file

---
+++ scrabble_test.rb
@@ -0,0 +1,34 @@
+require 'minitest/autorun'
+require_relative 'scrabble'
+
+class ScrabbleTest < Minitest::Test
+  def setup
+    @name = Scrabble.new
+  end
+
+  def test_scrabble_class_exists
+    assert_instance_of(Scrabble, @name)
+  end
+
+  def test_score_method_exists
+    assert_respond_to(@name, :score)
+  end
+
+  def test_score_method_exepects_string
+    assert_send([@name, :score, 'hello'])
+  end
+
+  def test_score_method_returns_correct_value
+    assert_equal(8, @name.score('hello'))
+    assert_equal(14, @name.score('cabbage'))
+  end
+
+  def test_score_method_returns_zero_when_argument_is_empty
+    assert_equal(0, @name.score(''))
+  end
+
+  def test_score_method_returns_zero_when_argument_is_nil
+    assert_equal(0, @name.score(nil))
+  end
+
+end
